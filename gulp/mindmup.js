ec67589997a5055a28e715c734509b6a81453f1f
itechdom <idea@itechdom.com>
2015-12-07 16:23:27 -0700

    added empty task for converting mindmups

---
+++ gulp/mindmup.js
@@ -0,0 +1,4 @@
+//mindmup conversion here
+//the only thing we need is to concatentate all the ideas together in one file
+//go through ideas one by one, traverse the tree till you get an attachment, find the title of that idea and add to the attachement
+//If there's no attachement, then just append the title
