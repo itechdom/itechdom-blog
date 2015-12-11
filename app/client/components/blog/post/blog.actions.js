0495088e5756c1c608ea0d942656fd9f6e52b00e
itechdom <idea@itechdom.com>
2015-12-11 12:57:30 -0700

    nested post

---
+++ app/client/components/blog/post/blog.actions.js
@@ -0,0 +1,20 @@
+'use strict';
+var Rx = require('rx');
+var dispatcher = require('../lib/dispatcher/dispatcher.js');
+var clientActions = require('../client/client.actions.js')
+
+
+class actionMain{
+
+    constructor(){
+        return {
+            request$: clientActions['changeRoute$'].filter((d)=> {
+                return d == "/todo";
+            }),
+            viewLoaded$: Rx.Observable.fromEvent(dispatcher.customEvent,'todo.viewLoaded$'),
+            dataLoaded$:Rx.Observable.fromEvent(dispatcher.customEvent,'todo.dataLoaded$'),
+	        addTodo$:Rx.Observable.fromEvent(dispatcher.customEvent,'todo.addTodo$')
+        }
+    }
+}
+module.exports = new actionMain();
