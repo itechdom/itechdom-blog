4c52d6e9cda59403913058a33074ca6da6beb7db
itechdom <idea@itechdom.com>
2015-12-13 15:14:14 -0700

    added user component

--- app/server/main.js
+++ app/server/main.js
@@ -1,5 +1,5 @@
 //init all data here
 var server = require('./components/utils/server/server.js');
-var todo = require('./components/todo/todo.js');
+var blog = require('./components/blog/blog.js');
 
 module.exports = server;
