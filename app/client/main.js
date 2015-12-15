11a65f40c16d313baf55485b088ce3f1efc3b591
itechdom <idea@itechdom.com>
2015-12-14 18:29:54 -0700

    added font-awesome

--- app/client/main.js
+++ app/client/main.js
@@ -1,4 +1,7 @@
 var Rx = require('rx');
+
 require("./src/scss/main.scss");
+require("font-awesome/scss/font-awesome.scss");
+
 require("./components/blog/blog.js");
 require("./components/admin/admin.js");
