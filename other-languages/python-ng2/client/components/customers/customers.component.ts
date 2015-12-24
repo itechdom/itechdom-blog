777da2cee009f53341d6d2f7b6e3a2138132287f
osama.alghanmi <osama.alghanmi@effectiveui.com>
2015-12-23 18:55:53 -0700

    moved angular template in

---
+++ other-languages/python-ng2/client/components/customers/customers.component.ts
@@ -0,0 +1,76 @@
+import { Component } from 'angular2/core';
+import { CORE_DIRECTIVES } from 'angular2/common';
+import { RouterLink } from 'angular2/router';
+//import { Observable } from 'rxjs/Observable';
+import { DataService } from '../../services/data.service';
+import { Sorter } from '../../utils/sorter';
+import { FilterTextboxComponent } from '../filterTextbox/filterTextbox.component';
+import { SortByDirective } from '../../directives/sortby.directive';
+import { CapitalizePipe } from '../../pipes/capitalize.pipe';
+
+@Component({ 
+  selector: 'customers', 
+  providers: [DataService],
+  templateUrl: 'app/components/customers/customers.component.html',
+  directives: [CORE_DIRECTIVES, RouterLink, FilterTextboxComponent, SortByDirective],
+  pipes: [CapitalizePipe]
+})
+export class CustomersComponent {
+
+  title: string;
+  filterText: string;
+  listDisplayModeEnabled: boolean;
+  customers: any[] = [];
+  filteredCustomers: any[] = [];
+  sorter: Sorter;
+
+  constructor(private dataService: DataService) { }
+  
+  ngOnInit() {
+    this.title = 'Customers';
+    this.filterText = 'Filter Customers:';
+    this.listDisplayModeEnabled = false;
+
+    this.dataService.getCustomers()
+        .subscribe((customers:any[]) => {
+          this.customers = this.filteredCustomers = customers;
+        });
+
+    this.sorter = new Sorter();
+  }
+
+  changeDisplayMode(mode: string) {
+      this.listDisplayModeEnabled = (mode === 'List');
+  }
+
+  filterChanged(data: string) {
+    if (data && this.customers) {
+        data = data.toUpperCase();
+        let props = ['firstName', 'lastName', 'address', 'city', 'orderTotal'];
+        let filtered = this.customers.filter(item => {
+            let match = false;
+            for (let prop of props) {
+                //console.log(item[prop] + ' ' + item[prop].toUpperCase().indexOf(data));
+                if (item[prop].toString().toUpperCase().indexOf(data) > -1) {
+                  match = true;
+                  break;
+                }
+            };
+            return match;
+        });
+        this.filteredCustomers = filtered;
+    }
+    else {
+      this.filteredCustomers = this.customers;
+    }
+  }
+
+  deleteCustomer(id: number) {
+
+  }
+
+  sort(prop: string) {
+      this.sorter.sort(this.filteredCustomers, prop);
+  }
+
+}
