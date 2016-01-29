9d8bc45190280d6c2e38bf6e77192423ee2aedb5
itechdom <osamah.net.m@gmail.com>
2016-01-28 22:05:53 -0700

    removed pre tags

--- gulp/mindmup.md.js
+++ gulp/mindmup.md.js
@@ -86,6 +86,9 @@ module.exports = function(options) {
 			html = html.replaceAll("</div>","");
 			html = html.replaceAll("<div>","");
 
+			html = html.replaceAll("</pre>","");
+			html = html.replaceAll("<pre>","");
+
 
 			html = html.replaceAll('<code class="js">',"```js\n");
 			html = html.replaceAll('<code>',"```\n");
