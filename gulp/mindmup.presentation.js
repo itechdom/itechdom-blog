42df9cd0a0e89468bfdb17c823e6872648e4d1ef
itechdom <osamah.net.m@gmail.com>
2016-01-28 22:03:14 -0700

    formatting markdown

--- gulp/mindmup.presentation.js
+++ gulp/mindmup.presentation.js
@@ -1,5 +1,3 @@
-//mindmup conversion here
-//the only thing we need is to concatentate all the ideas together in one file
 //go through ideas one by one, traverse the tree till you get an attachment, find the title of that idea and add to the attachement
 //If there's no attachement, then just append the title
 'use strict';
