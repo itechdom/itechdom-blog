a84a07d2cc3bfbc99410d1a12a3632fdb9c4a74d
itechdom <osamah.net.m@gmail.com>
2016-02-01 09:55:58 -0700

    hashtag

--- gulp/mindmup.md.js
+++ gulp/mindmup.md.js
@@ -92,7 +92,7 @@ module.exports = function(options) {
 			var f = "";
 			mindmap.map((idea)=>{
 				if(idea.title && idea.level){
-					var head = "#".repeat(idea.level);
+					var head = "# ".repeat(idea.level);
 					head += " ";
 					f+=head;
 					f+= idea.title;
