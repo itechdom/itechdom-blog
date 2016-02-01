e188edc771f2973fba49bba9b3f3f9c2624bafdb
itechdom <osamah.net.m@gmail.com>
2016-02-01 09:59:53 -0700

    head addition

--- gulp/mindmup.md.js
+++ gulp/mindmup.md.js
@@ -93,14 +93,14 @@ module.exports = function(options) {
 			mindmap.map((idea)=>{
 				if(idea.title && idea.level){
 					if(idea.level >= 0 && idea.level <= 3){
-					var head = "#".repeat(idea.level);
+					var head = "#".repeat(idea.level);;
 					head+=" ";
-					}
 					f+=head;
+					}
 					f+= idea.title;
 					f+="\n";
 				}
-				if(idea.content && idea.level){
+				if(idea.content){
 					f += idea.content;
 					f+="\n";
 				}
