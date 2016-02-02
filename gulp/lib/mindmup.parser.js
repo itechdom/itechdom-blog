8bbdc3aff47b7a66f6fe4f2832edf68ae057f524
itechdom <osamah.net.m@gmail.com>
2016-02-01 22:13:18 -0700

    create mindmup parser

---
+++ gulp/lib/mindmup.parser.js
@@ -0,0 +1,142 @@
+var cheerio = require('cheerio');
+var slideTemplate = `<div class="reveal">
+<div class="slides">
+</div>
+</div>`
+var slideElement = cheerio.load(slideTemplate);
+
+
+String.prototype.replaceAll = function(str1, str2, ignore) 
+{
+	return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+} 
+
+var ops = {
+	processItem(obj,key){
+		var pObject = {};
+		if(obj.title){
+			pObject.title = obj.title;
+		}
+		if(obj.attr){
+			var attr = obj.attr;
+			var attachment = attr.attachment;
+			if(attachment){
+				pObject.content = attachment.content;
+			}
+		}
+		pObject.key = parseFloat(key);
+		pObject.id = obj.id;
+		return pObject;
+	}
+	,traverse(mindmap,operation){
+
+	}
+	,flatten(mindmap,pArr,level){
+		for(var key in mindmap){
+			var obj = mindmap[key];
+			var pObject = processMindmapObject(obj,key);
+			if(!level){
+				pObject.level = 1;
+			}
+			else{
+				pObject.level = level;
+			}
+			pArr.push(pObject);
+			traverseMindmap(obj.ideas,pArr,levelsDeep++);
+		}
+	}
+	,sort(unordered){
+		var ordered = {};
+		var floats = {};
+		var arr = [];
+		Object.keys(unordered)
+			.sort(function(a,b){
+
+				floats.a = parseFloat(a);	
+				floats.b = parseFloat(b);
+
+				//when both numbers are above 0 (right side of the mindmap), then reverse sort
+				if(floats.a > 0 && floats.b > 0){
+					if(floats.a < floats.b){
+						return -1;
+					}
+					else{
+						return 1;
+					}
+				}
+				if(floats.a < floats.b){
+					return 1; 
+				}
+
+				if(floats.a > floats.b){
+					return -1;
+				}
+				return 0;
+			})	
+		.forEach(function(key) {
+			arr.push(unordered[key]);
+		});
+		return arr;
+	}
+	,parse(file){
+		var mindmap = JSON.parse(String(file.contents));
+		return mindmap;
+	}
+	,cleanHTML(html){
+		return html;
+	}
+	,convertToMarkdown(mindmap){
+		var f = "";
+		mindmap.map((idea)=>{
+			var head = "#"
+			if(idea.level){
+				head+="##";
+			}
+		head += " ";
+		f+=head;
+		f+= idea.title;
+		f+="\n";
+		if(idea.content){
+			f += idea.content;
+			f+="\n";
+		}
+		})
+		return f;
+	}
+	,convertToHTML(mindmap){
+		var f = "";
+		var count = 0;
+		var parentId;
+		mindmap.map((idea)=>{
+			var fruits = [];
+			var section;
+			var cHTML;
+			count++;
+			if(idea.title){
+				parentId = count;
+				var cleanTitle = idea.title.split(".")[1];
+				if(!cleanTitle)cleanTitle = idea.title;
+				slideElement('.slides').append(`<section class="parent${parentId}"><h3>${cleanTitle}</h3></section>`);
+			}
+			if(idea.content){
+				var cleanContent = cleanHTML(idea.content);
+				var code = cheerio.load(cleanContent);
+				var hCode;
+				code('*').each(function(i, elem) {
+					var codeBlock = code(this).find('code').html();
+					if(codeBlock){
+						hCode = codeBlock;
+					}
+				});
+				slideElement(`.parent${parentId}`).append('<br>'+cleanContent);
+				if(hCode){
+					slideElement(`.parent${parentId}`).find('code').addClass('prettyprint');
+					slideElement(`.parent${parentId}`).find('code').addClass('lang-js');
+				}
+			}
+		})
+		return slideElement.html();
+	}
+}
+module.exports = ops;
+
--- gulp/mindmup.md.js
+++ gulp/mindmup.md.js
@@ -1,130 +1,18 @@
-//mindmup conversion here
-//the only thing we need is to concatentate all the ideas together in one file
-//go through ideas one by one, traverse the tree till you get an attachment, find the title of that idea and add to the attachement
-//If there's no attachement, then just append the title
 'use strict';
-
 var gulp = require('gulp');
 var data = require('gulp-data');
 var path = require('path');
 var concat = require('gulp-concat');
-var flatten = require('flat');
 var rename = require("gulp-rename");
 var MongoClient = require('mongodb').MongoClient
-var cheerio = require('cheerio');
-var sortBy = require('sort-by');
-var mindmapAll;
-var levelsDeep;
-
-/**
- *
- * Best way to process this imo is to separate level 0 mindmap titles intot heir own 
- * Then apply some deep levels styling using one method, this should include appending the rest into the parent
- *
- *
- **/
+var mindmupParser = require('./lib/mindmup.parser.js');
 
 module.exports = function(options) {
 
 	gulp.task('mindmap:md', function(done) {
-
-		function processMindmapObject(obj,key){
-			var pObject = {};
-			if(obj.title){
-				pObject.title = obj.title;
-			}
-			if(obj.attr){
-				var attr = obj.attr;
-				var attachment = attr.attachment;
-				if(attachment){
-					pObject.content = attachment.content;
-				}
-			}
-			pObject.key = parseFloat(key);
-			pObject.id = obj.id;
-			return pObject;
-		}
-		function traverseMindmap(mindmap,pArr,level){
-			for(var key in mindmap){
-				var obj = mindmap[key];
-				var pObject = processMindmapObject(obj,key);
-				if(!level){
-					pObject.level = 1;
-				}
-				else{
-					pObject.level = level;
-				}
-				pArr.push(pObject);
-				traverseMindmap(obj.ideas,pArr,levelsDeep++);
-			}
-		}
-		function sortMindmap(unordered){
-			var ordered = {};
-			var floats = {};
-			var arr = [];
-			Object.keys(unordered)
-				.sort(function(a,b){
-
-					floats.a = parseFloat(a);	
-					floats.b = parseFloat(b);
-
-					//when both numbers are above 0 (right side of the mindmap), then reverse sort
-					if(floats.a > 0 && floats.b > 0){
-						if(floats.a < floats.b){
-							return -1;
-						}
-						else{
-							return 1;
-						}
-					}
-					if(floats.a < floats.b){
-						return 1; 
-					}
-
-					if(floats.a > floats.b){
-						return -1;
-					}
-					return 0;
-				})	
-			.forEach(function(key) {
-				arr.push(unordered[key]);
-			});
-			return arr;
-		}
-		function parseMindmap(file){
-			var mindmap = JSON.parse(String(file.contents));
-			return mindmap;
-		}
-		String.prototype.replaceAll = function(str1, str2, ignore) 
-		{
-			return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-		} 
-
-		function cleanHTML(html){
-			return html;
-		}
-
-		function convertToMarkdown(mindmap){
-			var f = "";
-			mindmap.map((idea)=>{
-				var head = "#"
-				if(idea.level){
-					head+="##";
-				}
-			head += " ";
-			f+=head;
-			f+= idea.title;
-			f+="\n";
-			if(idea.content){
-				f += idea.content;
-				f+="\n";
-			}
-			})
-			return f;
-		}
 		return gulp.src(options.drive+'/**/RxJS.presentation.mup')
 			.pipe(data(function(file) {
-				var mindmap = parseMindmap(file);
+				var mindmap = mindmup.parse(file);
 				var pArr = [];
 				var mindmapAll = mindmap.ideas;
 				levelsDeep = 0;
