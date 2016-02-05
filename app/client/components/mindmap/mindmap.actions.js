507945475f2e60078445823da9189c29f31973d1
itechdom <osamah.net.m@gmail.com>
2016-02-04 20:17:42 -0700

    removed lib/deps

--- app/client/components/mindmap/mindmap.actions.js
+++ app/client/components/mindmap/mindmap.actions.js
@@ -1,5 +1,1 @@
-global.jQuery = global.$ = require('jquery');
-global._ = require('underscore');
-global.Color = require('color');
-global.Hammer = require('jquery-hammerjs/jquery.hammer.js');
-require('jquery.hotkeys');
+
