77a0bb32deb5eb7a7d6c9afa6ce4479107a95666
itechdom <osamah.net.m@gmail.com>
2016-02-12 16:27:45 -0700

    added blog gulp task

---
+++ gulp/mindmup.blog.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var data = require('gulp-data');
+var concat = require('gulp-concat');
+var MongoClient = require('mongodb').MongoClient
+var url;
+var mindmupParse = require('../gulp-custom/mindmup.parser.js')
+
+module.exports = function(options) {
+
+	gulp.task('mindmap:blog', function(done) {
+
+		return gulp.src(options.drive+"**/*.blog.mup")
+			.pipe(data(function(file) {
+				var mindmap = mindmupParse.toJson(file);
+				var pArr = [];
+				const unordered = mindmap.ideas;	
+				var ordered = mindmupParse.sort(unordered);
+				mindmupParse.clean(ordered);
+				var finalContent = JSON.stringify(ordered);
+				file.contents = new Buffer(finalContent);
+			}))
+			.pipe(gulp.dest('./content/published-blog'))
+			});
+};
