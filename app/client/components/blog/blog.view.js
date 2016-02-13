b066e6e744c3449671b0e50a639c51a32eb5d588
itechdom <osamah.net.m@gmail.com>
2016-02-12 17:10:17 -0700

    added hscript

--- app/client/components/blog/blog.view.js
+++ app/client/components/blog/blog.view.js
@@ -2,14 +2,12 @@
 var $ = require('jquery');
 var Rx = require('rx');
 var dispatcher = require('../../lib/dispatcher/dispatcher.js');
-var header = require('../../src/views/blog/header.html');
-var content = require('../../src/views/blog/index.html');
-var footer = require('../../src/views/blog/footer.html');
 var template  = require('./blog.template.js');
 var h = require('virtual-dom/h');
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
 var createElement = require('virtual-dom/create-element');
+
 
 class blogView {
 
@@ -19,11 +17,6 @@ class blogView {
 		var tree = template(count);               // We need an initial tree
 		var rootNode = createElement(tree);     // Create an initial root DOM node ...
 		document.body.appendChild(rootNode);    // ... and it should be in the document
-
-		$('app').html(template);
-		$('blog-header').html(header);
-		$('blog').html(content);
-		$('blog-footer').html(footer);
 
 		setInterval(function () {
 			count++;
