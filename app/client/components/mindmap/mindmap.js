6a3f47865f6681e2161f72de292887dec509d2c7
Osama Alghanmi <osamah.net.m@gmail.com>
2016-02-18 23:20:21 -0700

    fixed container issue

--- app/client/components/mindmap/mindmap.js
+++ app/client/components/mindmap/mindmap.js
@@ -8,13 +8,16 @@ require('jquery.hotkeys');
 var view = require('./mindmapView/mindmapView.view.js');
 var actions = require('./mindmap.actions.js');
 var model = require('../../../../content/json-blog/Knowledge/Formal\ Science/Computer\ Science/Elm/Elm.mup.json');
+var draggable = require('./mindmapView/mindmapDraggable.view.js');
+
 
 class mindmapMain{
 	constructor(){
 		var jModel = JSON.parse(model);
 		actions.request$.subscribe(()=>{
-			$('app').html();
-			view.render(jModel);
+		$('app').html();
+		view.render(jModel);
+		draggable(100,100);
 		})
 	}
 }
--- app/client/components/mindmap/mindmapView/mindmapDraggable.view.js
+++ app/client/components/mindmap/mindmapView/mindmapDraggable.view.js
@@ -2,32 +2,59 @@
 var template = require('./mindmapView.html');
 var $ = require('jquery');
 var PIXI = require('pixi.js');
+var renderer = PIXI.autoDetectRenderer(1000, 1000, { antialias: true });
+document.body.appendChild(renderer.view);
+var stage = new PIXI.Container();
+stage.interactive = true;
 
-class mindmapView {
-	function createBunny(x, y)
-	{
-		// setup events
-		bunny
-			// events for drag start
-			.on('mousedown', onDragStart)
-			.on('touchstart', onDragStart)
-			// events for drag end
-			.on('mouseup', onDragEnd)
-			.on('mouseupoutside', onDragEnd)
-			.on('touchend', onDragEnd)
-			.on('touchendoutside', onDragEnd)
-			// events for drag move
-			.on('mousemove', onDragMove)
-			.on('touchmove', onDragMove);
 
-		// move the sprite to its designated position
-		bunny.position.x = x;
-		bunny.position.y = y;
+function createBox(x, y)
+{
+	// setup events
+	var style = {
+		font : 'bold italic 10px Arial',
+		fill : '#F7EDCA',
+		wordWrap : true,
+		wordWrapWidth : 440
+	};
 
-		// add it to the stage
-		stage.addChild(bunny);
-	}
+	var basicText = new PIXI.Text("hello",style);
+	basicText.x = x;
+	basicText.y = y;
 
+	var container = new PIXI.Container();
+
+	var box = new PIXI.Graphics();
+	box.lineStyle(2, 0x0000FF, 1);
+	box.beginFill(0xFF700B, 1);
+	box.drawRect(0, 0, 20, 20);
+
+	// enable the box to be interactive... this will allow it to respond to mouse and touch events
+	box.interactive = true;
+
+	// this button mode will mean the hand cursor appears when you roll over the box with your mouse
+	box.buttonMode = true;
+
+	box
+		// events for drag start
+		.on('mousedown', onDragStart)
+		.on('touchstart', onDragStart)
+		// events for drag end
+		.on('mouseup', onDragEnd)
+		.on('mouseupoutside', onDragEnd)
+		.on('touchend', onDragEnd)
+		.on('touchendoutside', onDragEnd)
+		// events for drag move
+		.on('mousemove', onDragMove)
+		.on('touchmove', onDragMove);
+
+
+	container.x = x;
+	container.y = y;
+
+
+	container.addChild(box);
+	stage.addChild(container);
 	requestAnimationFrame( animate );
 
 	function animate() {
@@ -36,34 +63,40 @@ class mindmapView {
 		renderer.render(stage);
 	}
 
-	function onDragStart(event)
+}
+
+function onDragStart(event)
+{
+	// store a reference to the data
+	// the reason for this is because of multitouch
+	// we want to track the movement of this particular touch
+	this.data = event.data;
+	console.log(event);
+	this.alpha = 0.5;
+	this.dragging = true;
+}
+
+function onDragEnd()
+{
+	this.alpha = 1;
+
+	this.dragging = false;
+
+	// set the interaction data to null
+	this.data = null;
+}
+
+function onDragMove()
+{
+	if (this.dragging)
 	{
-		// store a reference to the data
-		// the reason for this is because of multitouch
-		// we want to track the movement of this particular touch
-		this.data = event.data;
-		this.alpha = 0.5;
-		this.dragging = true;
-	}
-
-	function onDragEnd()
-	{
-		this.alpha = 1;
-
-		this.dragging = false;
-
-		// set the interaction data to null
-		this.data = null;
-	}
-
-	function onDragMove()
-	{
-		if (this.dragging)
-		{
-			var newPosition = this.data.getLocalPosition(this.parent);
-			this.position.x = newPosition.x;
-			this.position.y = newPosition.y;
-		}
+		console.log(this.parent);
+		var newPosition = this.data.getLocalPosition(this.parent);
+		console.log(newPosition);
+		this.position.x = newPosition.x;
+		this.position.y = newPosition.y;
+		//this.x = newPosition.x;
+		//this.y = newPosition.y;
 	}
 }
-module.exports = new mindmapView();
+module.exports = createBox;
