3312245665ed6874f7919475bcc4197205ea33c9
Osama Alghanmi <osamah.net.m@gmail.com>
2016-02-23 18:21:49 -0700

    complete code splitting for files

--- app/client/components/mindmap/mindmap.js
+++ app/client/components/mindmap/mindmap.js
@@ -4,21 +4,19 @@ global.jQuery = global.$ = require('jquery');
 //global.Color = require('color');
 //global.Hammer = require('jquery-hammerjs/jquery.hammer.js');
 //require('jquery.hotkeys');
-
 var view = require('./mindmapView/mindmapView.view.js');
 var actions = require('./mindmap.actions.js');
-var model = require('../../../../content/json-blog/Knowledge/Formal\ Science/Computer\ Science/Elm/Elm.mup.json');
+var model = require('./Elm.mup.json');
 var draggable = require('./mindmapView/mindmapDraggable.view.js');
 
+console.log(model);
 
 class mindmapMain{
 	constructor(){
 		var jModel = JSON.parse(model);
-		actions.request$.subscribe(()=>{
 		$('app').html();
 		view.render(jModel);
 		draggable(100,100);
-		})
 	}
 }
 
