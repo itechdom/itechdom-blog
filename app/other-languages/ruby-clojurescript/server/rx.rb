fc4960438be7ae9eaa0ac5184b0631cfe0cc9370
Osama Alghanmi <osamah.net.m@gmail.com>
2016-02-24 21:14:12 -0700

    added more rx examples

--- app/other-languages/ruby-clojurescript/server/rx.rb
+++ app/other-languages/ruby-clojurescript/server/rx.rb
@@ -1,8 +1,17 @@
 require 'rx_ruby'
+
 a = Rx::Observable.from_array [ 4, 5, 6 ]
 b = Rx::Observable.from_array [ 7, 8, 9 ]
 
-sub = a.zip(b).subscribe {|arr| puts arr.to_s }
+sub  = Rx::Subject.new
+
+
+b.subscribe(sub)
+
+a.subscribe {|data| sub.on_next(data)}
+
+
+sub = a.subscribe {|arr| puts arr.to_s }
 
 # => "[4, 7]"
 # # => "[5, 8]"
