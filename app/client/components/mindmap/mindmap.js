c5405826128b0c53878c37916cd139ccb73eb6fd
Osama Alghanmi <osamah.net.m@gmail.com>
2016-02-26 20:08:33 -0700

    updated package.json

--- app/client/components/mindmap/mindmap.js
+++ app/client/components/mindmap/mindmap.js
@@ -1,9 +1,4 @@
 'use strict';
-global.jQuery = global.$ = require('jquery');
-//global._ = require('underscore');
-//global.Color = require('color');
-//global.Hammer = require('jquery-hammerjs/jquery.hammer.js');
-//require('jquery.hotkeys');
 var view = require('./mindmapView/mindmapView.view.js');
 var actions = require('./mindmap.actions.js');
 var model = require('./Elm.mup.json');
