7b0326d68a0e29ee7f2b2e5011f4cae9de588fe0
itechdom <osamah.net.m@gmail.com>
2016-03-28 01:12:32 -0400

    docker file and package update

--- node-docker.sh
+++ node-docker.sh
@@ -1,2 +1,2 @@
 docker build -t my-nodejs-app .
-docker run -d -p 49160:8080 --name node-running-app -v /root/itechdom-blog:/src/app my-nodejs-app
+docker run -d -p 49160:3000 --name node-running-app -v /root/itechdom-blog:/src/app my-nodejs-app
