1ee8a1fa512ee70266fac71dd9ed2734c118c44f
itechdom <osamah.net.m@gmail.com>
2016-05-19 18:22:55 -0600

    added dom files to display maps

---
+++ app/client/components/mindmap/mindmapView/mindmapView.view.dom.js
@@ -0,0 +1,7 @@
+'use strict';
+var template = require('./mindmapView.html');
+var $ = require('jquery');
+class mindmapView {
+
+}
+module.exports = new mindmapView();
--- app/client/main.js
+++ app/client/main.js
@@ -2,9 +2,9 @@
 //require("./components/admin/admin.js")
 //require("./components/post/post.js");
 //require("./components/slides/slides.js");
-require("./components/blog/blog.js");
+//require("./components/blog/blog.js");
 //require("./components/admin/admin.actions.js");
 //require("./components/post/post.actions.js");
 //require("./components/slides/slides.actions.js");
 //require("./components/todo/todo.actions.js");
-//require("./components/mindmap/mindmap.actions.js");
+require("./components/mindmap/mindmap.actions.js");
