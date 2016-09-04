0f223000b41d40b5b6177067f28a2b74ff88145a
itechdom <osamah.net.m@gmail.com>
2016-09-03 21:23:23 -0600

    blog

--- app/client/main.js
+++ app/client/main.js
@@ -2,12 +2,12 @@
 //require("./components/admin/admin.js")
 //require("./components/post/post.js");
 //require("./components/slides/slides.js");
-//require("./components/blog/blog.js");
+require("./components/blog/blog.js");
 //require("./components/admin/admin.actions.js");
-//require("./components/post/post.actions.js");
+require("./components/post/post.actions.js");
 //require("./components/slides/slides.actions.js");
 //require("./components/todo/todo.actions.js");
 //we are loading actions here because we are using webpack's async file load capability
-require("./components/mindmap/mindmap.actions.js");
-require("./components/react/index.js");
+//require("./components/mindmap/mindmap.actions.js");
+//require("./components/react/index.js");
 //require("./components/3d/3d.js");
