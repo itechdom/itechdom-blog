2ff2e00f546deb8bcc0aa6d7c1426b02b2236081
itechdom <osamah.net.m@gmail.com>
2016-09-07 00:03:58 -0600

    attempting native promises

--- app/server/nativePromise.js
+++ app/server/nativePromise.js
@@ -9,6 +9,7 @@ class Promise {
     }
     then(args){
         if(typeof args === 'function'){
+            this.then = args;
             let p = args();
             return p;
         }
@@ -21,7 +22,6 @@ class Promise {
     }
 }
 function testPromise(){
-    console.log("finito");
     let pr = new Promise();
     setTimeout(()=>{
         pr.resolve('message');
@@ -29,6 +29,7 @@ function testPromise(){
     return pr;
 }
 let a = testPromise().then((res)=>{
+    console.log(res);
     let pr2 = new Promise();
     setTimeout(()=>{
         pr2.resolve('message2');
@@ -36,5 +37,5 @@ let a = testPromise().then((res)=>{
     return pr2;
 })
 a.then((t)=>{
-    console.log("finito");
+    console.log(t);
 })
