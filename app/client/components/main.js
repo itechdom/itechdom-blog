71634d2d4befe8316d0781fd78e16ace4568934e
itechdom <osamah.net.m@gmail.com>
2016-09-17 11:17:01 -0600

    installed react dom

--- app/client/components/main.js
+++ app/client/components/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import HelloWorld from './HelloWorld.js';
 
-React.render(<HelloWorld />,'body');
+ReactDOM.render(<HelloWorld />,'body');
