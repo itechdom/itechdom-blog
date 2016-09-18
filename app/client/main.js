e05aa1e5adf9ddb330b1d5eaeb3f653c285d7cbc
itechdom <osamah.net.m@gmail.com>
2016-09-18 10:35:10 -0600

    added cyclejs

--- app/client/main.js
+++ app/client/main.js
@@ -1,1 +1,18 @@
-require('./components/main.js');
+import xs from 'xstream';
+import {run} from '@cycle/xstream-run';
+import {makeDOMDriver, h1} from '@cycle/dom';
+
+function main() {
+    const sinks = {
+        DOM: xs.periodic(1000).map(i =>
+                                   h1('' + i + ' seconds elapsed')
+                                  )
+    };
+    return sinks;
+}
+
+const drivers = {
+    DOM: makeDOMDriver('#app')
+};
+
+run(main, drivers);
