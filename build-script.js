659d936a3d4b1eb9093c30d2d29941b463d7d405
itechdom <osamah.net.m@gmail.com>
2016-09-24 13:57:17 -0600

    build completed

--- build-script.js
+++ build-script.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 const pages = {
-    './pages/':['about.html','contact.html','home.html','case-studies.html'],
-    './pages/case-studies/':['meetup.html','landdox.html','tx.html']
+    './components/':['about.html','contact.html','home.html','case-studies.html'],
+    './components/case-studies/':['meetup.html','landdox.html','tx.html']
 }
+const buildDir = './pages/'
 const index = String(fs.readFileSync('./index.html'));
 let mainPages = Object.keys(pages).map((key)=>{
     let arr = pages[key];
     arr.map((page)=>{
         let replacedPage = index.replace('{content}',String(fs.readFileSync(key+page)));
-        fs.writeFileSync('./build/'+page,replacedPage);
+        fs.writeFileSync(buildDir+page,replacedPage);
     })
 })
