a05e70ed52b6dd8f42ce1b0557b1865a80d3484e
itechdom <osamah.net.m@gmail.com>
2016-09-24 15:50:03 -0600

    loading site locally is possible

--- build-script.js
+++ build-script.js
@@ -3,12 +3,15 @@ const pages = {
     './components/':['about.html','contact.html','home.html','case-studies.html'],
     './components/case-studies/':['meetup.html','landdox.html','tx.html']
 }
-const buildDir = './pages/'
+const buildDir = './build/'
 const index = String(fs.readFileSync('./index.html'));
+console.log("one");
 let mainPages = Object.keys(pages).map((key)=>{
     let arr = pages[key];
     arr.map((page)=>{
         let replacedPage = index.replace('{content}',String(fs.readFileSync(key+page)));
-        fs.writeFileSync(buildDir+page,replacedPage);
+        fs.writeFileSync(buildDir+'pages/'+page,replacedPage);
     })
 })
+console.log("two");
+fs.createReadStream('index.html').pipe(fs.createWriteStream(buildDir+'index.html'));
