bc08725f3046257e20523998ed658fc31118fa3e
itechdom <osamah.net.m@gmail.com>
2016-09-25 19:50:30 -0600

    hash table

--- app/client/main.js
+++ app/client/main.js
@@ -1,1 +1,24 @@
-require('./components/main.js');
+let key, hashNumber, arr = [];
+
+function computeHashNumber(key){
+    let iterator = key[Symbol.iterator]();
+    let code = 0;
+    for(value of iterator){
+        code += value.charCodeAt();
+    }
+    return code;
+}
+function compouteIndex(number){
+    return number % arr.length;
+}
+function lookup(key){
+
+}
+function insert(key,value){
+    let number = computeHashNumber(key);
+    let index = computeIndex(number);
+    arr[index] = {'${key}':value }
+}
+function delete(key){
+
+}
