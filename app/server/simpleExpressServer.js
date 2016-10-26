6caf61553538b96f55d3399f74348640c7023586
Sam Alghanmi <oalghnmi@gmail.com>
2016-10-26 11:19:56 -0600

    finished simple socket io example

--- app/server/simpleExpressServer.js
+++ app/server/simpleExpressServer.js
@@ -8,6 +8,13 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message',msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
