20efb15ea882d303bb4d8e53dd31c783eebd47a8
itechdom <oalghnmi@gmail.com>
2016-11-03 21:19:59 -0600

    added async await example

--- app/server/asyncAwaitExamples/awsEmailService.js
+++ app/server/asyncAwaitExamples/awsEmailService.js
@@ -1,16 +1,10 @@
-let AWS = require('aws-sdk');
-let config = require('config');
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 var Promise = require('bluebird');
 
 //TODO: import this from thinktank-common later if it makes sense
 export class AWSEmail {
-  ses;
   constructor() {
-    this.ses = new AWS.SES({
-      region: config.get('aws.region')
-    });
   }
   getAWSEmail(emailObject){
     return {
@@ -32,14 +26,16 @@ export class AWSEmail {
           Data: emailObject.subject,
           Charset: 'UTF-8'
         }
-      },
-      Source: config.get('email.source')
+      }
     }
+  }
+  sendEmail(awsEmail,callback){
+      callback(undefined,'email sent');
   }
   send(emailObject, callback) {
     let awsEmail = this.getAWSEmail(emailObject);
     return new Promise((resolve, reject) => {
-        this.ses.sendEmail(awsEmail, (err, data)=>{
+        this.sendEmail(awsEmail, (err, data)=>{
             if(err){
                 reject(err);
             }
