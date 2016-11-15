9d07eada7fb256ffb014ef80ca6241175f46eb67
Sam Alghanmi <oalghnmi@gmail.com>
2016-11-14 22:09:47 -0700

    fsharp compile

---
+++ app/fsharp/obj/Debug/netcoreapp1.0/dotnet-compile.assemblyinfo.fs
@@ -0,0 +1,16 @@
+namespace System
+
+
+
+
+[<assembly:System.Reflection.AssemblyTitleAttribute("")>]
+[<assembly:System.Reflection.AssemblyDescriptionAttribute("")>]
+[<assembly:System.Reflection.AssemblyCopyrightAttribute("")>]
+[<assembly:System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
+[<assembly:System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
+[<assembly:System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly:System.Reflection.AssemblyCultureAttribute("")>]
+[<assembly:System.Resources.NeutralResourcesLanguageAttribute("")>]
+[<assembly:System.Runtime.Versioning.TargetFrameworkAttribute(".NETCoreApp,Version=v1.0")>]
+do ()
+
