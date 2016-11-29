a7de997fdd6e94ffd500ffe36397428c2756a907
Sam Alghanmi <oalghnmi@gmail.com>
2016-11-29 13:14:27 -0700

    increased viewport size

--- app/client/components/mindmap/mindmapView/canvas/mindmap.canvas.js
+++ app/client/components/mindmap/mindmapView/canvas/mindmap.canvas.js
@@ -36,7 +36,7 @@ class mindmapView {
 		return style;
 	}
 	createBox(){
-        var shadowFilter = new PIXI.filters.DropShadowFilter();
+		var shadowFilter = new PIXI.filters.DropShadowFilter();
 		var box = new PIXI.Graphics();
 		box.lineStyle(2, 0x000000, 1);
 		box.beginFill(0xFFFFFF, 1);
@@ -45,94 +45,94 @@ class mindmapView {
 	}
 	traverse(mindmap,processFunction,parent){
 		var obj;
-        var count = 0;
-        //return upper sibling and below sibling
+		var count = 0;
+		//return upper sibling and below sibling
 		for(var key in mindmap){
 			obj = mindmap[key];
-            obj.order = count;
+			obj.order = count;
 			processFunction(obj,key,parent);
 			this.traverse(obj.ideas,processFunction,obj);
-            count++;
+			count++;
 		}
 	}
-    drawLine(childContainer,parentContainer,childBox,parentBox){
-        var line;
-        //delete the line if it already exists
-        if(line){
-            this.stage.removeChild(line);
-        }
-        //FIX: for a quirk in pixi.js container coordinates
-        if(childContainer.toGlobal(this.stage.position).y === 0){
-            var childBoxPosition = childContainer.toGlobal(this.rootContainer.position);
-            var parentBoxPosition = parentContainer.toGlobal(this.stage.position);
-        }
-        else{
-            var childBoxPosition = childContainer.toGlobal(this.stage.position);
-            var parentBoxPosition = parentContainer.toGlobal(this.stage.position);
-        }
+	drawLine(childContainer,parentContainer,childBox,parentBox){
+		var line;
+		//delete the line if it already exists
+		if(line){
+			this.stage.removeChild(line);
+		}
+		//FIX: for a quirk in pixi.js container coordinates
+		if(childContainer.toGlobal(this.stage.position).y === 0){
+			var childBoxPosition = childContainer.toGlobal(this.rootContainer.position);
+			var parentBoxPosition = parentContainer.toGlobal(this.stage.position);
+		}
+		else{
+			var childBoxPosition = childContainer.toGlobal(this.stage.position);
+			var parentBoxPosition = parentContainer.toGlobal(this.stage.position);
+		}
 
-        childBoxPosition.x += childBox.x;
-        childBoxPosition.y += childBox.y;
-        parentBoxPosition.x += parentBox.x;
-        parentBoxPosition.y += parentBox.y;
+		childBoxPosition.x += childBox.x;
+		childBoxPosition.y += childBox.y;
+		parentBoxPosition.x += parentBox.x;
+		parentBoxPosition.y += parentBox.y;
 
-        line = this.createLine(childBoxPosition,parentBoxPosition);
-        this.stage.addChild(line);
-        return line;
-    }
-    update(node){
-        var obj;
-        var parent = node.obj;
+		line = this.createLine(childBoxPosition,parentBoxPosition);
+		this.stage.addChild(line);
+		return line;
+	}
+	update(node){
+		var obj;
+		var parent = node.obj;
 		//change children
 		for (var key in node.obj.ideas){
-            obj = node.obj.ideas[key];	
-            if(parent.line){
-                this.drawLine(obj.mainContainer,parent.mainContainer,obj.line);
-            }
-            obj.box.x = obj.box.x + 3;
+			obj = node.obj.ideas[key];
+			if(parent.line){
+				this.drawLine(obj.mainContainer,parent.mainContainer,obj.line);
+			}
+			obj.box.x = obj.box.x + 3;
 		};
-    }
+	}
 	render(tree){
 
 		this.tree = tree;
 		var x,y;
-        var length;
-        var mainContainer;
-        var box;
-        var sText;
-        var text;
-        var sibling;
-        var siblingHeight = 0;
-        var arrangement;
-        var debugRect;
-        this.currentHeight = 0;
-        var gPosition = {y:0};
-        var line;
-        var that = this;
+		var length;
+		var mainContainer;
+		var box;
+		var sText;
+		var text;
+		var sibling;
+		var siblingHeight = 0;
+		var arrangement;
+		var debugRect;
+		this.currentHeight = 0;
+		var gPosition = {y:0};
+		var line;
+		var that = this;
 		this.traverse(tree,(mindmapObj,key,parent)=>{
 
 			box = this.createBox();
-            box.interactive = true;
-            mainContainer = new PIXI.Container();
-            debugRect = new PIXI.Graphics();
+			box.interactive = true;
+			mainContainer = new PIXI.Container();
+			debugRect = new PIXI.Graphics();
 
-            mindmapObj.mainContainer = mainContainer;
-            mindmapObj.box = box;
+			mindmapObj.mainContainer = mainContainer;
+			mindmapObj.box = box;
 
 			sText = mindmapObj.title.slice(0,10);
 			text = this.createText(sText);
-            if(text.width > box.width){
-                box.width = text.width;
-            }
-            else{
-                text.y += 10
-            }
-            //store a reference to the object here to be used when updating the object's position
-            box.obj = mindmapObj;
+			if(text.width > box.width){
+				box.width = text.width;
+			}
+			else{
+				text.y += 10
+			}
+			//store a reference to the object here to be used when updating the object's position
+			box.obj = mindmapObj;
 			box.addChild(text);
-            mainContainer.addChild(box);
+			mainContainer.addChild(box);
 
-            // events for drag start
+			// events for drag start
 			box
 			.on('mousedown', onDragStart)
 			.on('touchstart', onDragStart)
@@ -145,48 +145,50 @@ class mindmapView {
 			.on('mousemove', onDragMove)
 			.on('touchmove', onDragMove);
 
-            if(parent){
-
-                parent.mainContainer.addChild(mainContainer);
-
-                //get previous sibling
-                var count = 0;
-                Object.keys(parent.ideas).map((key,index)=>{
-                    if(index === mindmapObj.order){
-                        var order = parseInt(key) - 1;
-                        sibling = parent.ideas[`${order}`];
-                    }
-                    count++;
-                });
-                length = count; 
-                if(sibling){
-                    mainContainer.x += HORIZONTAL_MARGIN;
-                    mainContainer.y = sibling.mainContainer.y + sibling.mainContainer.height + VERTICAL_MARGIN;
-                    //move parent to fit children
-                    //var h = parent.ideas[mindmapObj.order+1];
-                    //if(h){
-                      //  if((mindmapObj.order+1)/length === 0.5){
-                            //parent.box.y = mainContainer.y;
-                        //}
-                    //}
-                    //else{
-                      //  console.log("nope");
-                   // }
-                }
-                else{
-                    mainContainer.x += HORIZONTAL_MARGIN;
-                    //mainContainer.y = 20;
-                }
-                if(mindmapObj.title === "Resources"){
-                }
-                //drawLine
-                var po = box.toLocal(mainContainer.position);
-                this.drawLine(mainContainer,parent.mainContainer,box,parent.box);
-                mindmapObj.line = line;
-            }
-            else{
-                this.rootContainer.addChild(mainContainer);
-            }
+			if(parent){
+				parent.mainContainer.addChild(mainContainer);
+				//get previous sibling
+				var count = 0;
+				var order;
+				Object.keys(parent.ideas).map((key,index)=>{
+					if(index === mindmapObj.order){
+						order = parseInt(key) - 1;
+						sibling = parent.ideas[`${order}`];
+					}
+					count++;
+				});
+				length = count;
+				if(sibling){
+					mainContainer.x += HORIZONTAL_MARGIN;
+					mainContainer.y = sibling.mainContainer.y + sibling.mainContainer.height + VERTICAL_MARGIN  ;
+					//move parent to fit children
+					//var h = parent.ideas[mindmapObj.order+1];
+					//if(h){
+					//  if((mindmapObj.order+1)/length === 0.5){
+					//parent.box.y = mainContainer.y;
+					//}
+					//}
+					//else{
+					//  console.log("nope");
+					// }
+				}
+				else{
+					mainContainer.x += HORIZONTAL_MARGIN;
+					//console.log(((-1 * Math.floor(length/2)) + order) * VERTICAL_MARGIN);
+					mainContainer.y = ((-1 * Math.floor(length/2)) + order) * VERTICAL_MARGIN;
+				}
+				//for debugging
+				if(mindmapObj.title === "Resources"){
+					console.log(((-1 * Math.floor(length/2)) + order) * VERTICAL_MARGIN);
+				}
+				//drawLine
+				var po = box.toLocal(mainContainer.position);
+				this.drawLine(mainContainer,parent.mainContainer,box,parent.box);
+				mindmapObj.line = line;
+			}
+			else{
+				this.rootContainer.addChild(mainContainer);
+			}
 		})
 		function onDragStart(event)
 		{
@@ -220,9 +222,9 @@ class mindmapView {
 	}
 	constructor() {
 
-		this.renderer = PIXI.autoDetectRenderer(1000, 1000, { antialias: true });
-        this.renderer.autoResize = true;
-        this.renderer.backgroundColor = 0x00BFFF;
+		this.renderer = PIXI.autoDetectRenderer(8000, 8000, { antialias: true });
+		this.renderer.autoResize = true;
+		this.renderer.backgroundColor = 0x00BFFF;
 
 		$('app').append(this.renderer.view);
 
@@ -230,21 +232,21 @@ class mindmapView {
 		this.rootContainer = new PIXI.Container();
 		this.stage.interactive = true;
 
-        //add a container to the center of the screen
-        this.rootContainer.x = this.renderer.width /3;
-        this.rootContainer.y = this.renderer.height / 6;
+		//add a container to the center of the screen
+		this.rootContainer.x = this.renderer.width /3;
+		this.rootContainer.y = this.renderer.height / 6;
 
-        this.stage.addChild(this.rootContainer);
+		this.stage.addChild(this.rootContainer);
 
-        this.renderer.render(this.stage);
+		this.renderer.render(this.stage);
 
-        requestAnimationFrame( animate );
-        var that = this;
+		requestAnimationFrame( animate );
+		var that = this;
 
-        function animate() {
-            requestAnimationFrame(animate);
-            that.renderer.render(that.stage);
-        }
-    }
+		function animate() {
+			requestAnimationFrame(animate);
+			that.renderer.render(that.stage);
+		}
+	}
 }
 module.exports = new mindmapView();
