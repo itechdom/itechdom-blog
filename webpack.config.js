5095dc753cc24b780d98c56565abc39300e24e78
Sam Alghanmi <oalghnmi@gmail.com>
2016-12-08 11:48:35 -0700

    a better angular and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,19 @@
-var path = require("path");
-var webpack = require("webpack");
-module.exports = {
-	entry: {
-		dist: './app/client/main.js'
-	},
-	output: {
-		path: __dirname + "/dist",
-		publicPath: "/dist/",
-		filename: '[name].js'
-	},
-	eslint: {
-		configFile: './eslintrc.json'
-	},
-	resolve: {
-        extensions:['','.js','.jsx','.tsx','.ts'],
-		modulesDirectories: ['app', 'node_modules', 'base_modules']
-	},
-	module: {
-		preLoaders: [{
-			test: /\.json$/,
-			loader: 'json-loader'
-		}],
-		loaders: [
-			{test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loaders: ['eslint-loader','babel']},
-			{test: /\.html$/, loader: 'raw'},
-			{test: /\.css$/, loader: "style-loader!css-loader"},
-			{
-				test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-				loader: 'file-loader'
-			},
-			{
-				test: /\.scss$/,
-                loaders: ["style", "css", "sass"]	
-			},
-			{
-				test: /\.md$/,
-				loader: "html!markdown"
-			},
-            {
-                test: /\.jsx?$/,
-                loaders: ['babel']
-            },
-            { test: /\.tsx?$/, loader: 'ts-loader' }
-        ],
-            postLoaders:[
-            {
-                include: path.resolve(__dirname, 'node_modules/pixi.js'),
-				loader: 'transform?brfs'
-			}
-		]
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			// Automtically detect jQuery and $ as free var in modules
-			// and inject the jquery library
-			// This is required by many jquery plugins
-			jQuery: "jquery",
-			$: "jquery"
-		})
-	]
-};
+/**
+ * @author: @AngularClass
+ */
+
+// Look in ./webpack-config folder for webpack.dev.js
+switch (process.env.NODE_ENV) {
+  case 'prod':
+  case 'production':
+    module.exports = require('./webpack-config/webpack.prod')({env: 'production'});
+    break;
+  case 'test':
+  case 'testing':
+    module.exports = require('./webpack-config/webpack.test')({env: 'test'});
+    break;
+  case 'dev':
+  case 'development':
+  default:
+    module.exports = require('./webpack-config/webpack.dev')({env: 'development'});
+}
